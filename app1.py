f4c3e8575674576f5b6409af374708ebd40f46df
Marcello Caccamo <londonliving89@gmail.com>
2022-04-29 16:29:35 +1000

    tranlater app

---
+++ app1.py
@@ -0,0 +1,33 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def tranlsate(w):
+    if w in data:
+        return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if Yes, or N if No: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else: 
+            return "We didn't understand your query"
+    else:
+        return "The word doesn't exist. Please double check it"
+
+    
+word = input("Enter word: ")
+
+output = (tranlsate(word))
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
